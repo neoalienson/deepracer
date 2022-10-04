3c13c57fbefb1e14152f40f5543f70cbba3b100f
Neo Alienson <neo@01man.com>
2022-10-05 00:44:03 +0800

    WIP

--- reward_function.py
+++ reward_function.py
@@ -330,7 +330,7 @@ def print_params():
         print(f'dc: {P.distance_from_center:.2f}, p:{P.progress:.2f}, st:{P.steps:3.0f}, cw:{P.closest_waypoints}, dd:{G.direction_diff:.1f}, rd:{G.route_direction:.1f}, aw: {P.all_wheels_on_track}, il: {P.is_left_of_center}, 2ox:{G.optimals_second[0]}, 2oy:{G.optimals_second[1]}')
         print(f'ot: {P.is_offtrack}, tw: {P.track_width:.2f}')
 
-    print(f"{{'all_wheels_on_track': {P.all_wheels_on_track},'x':{P.x},'y':{P.y},'distance_from_center': {P.distance_from_center},'is_left_of_center': {P.is_left_of_center},'heading': {P.heading},'progress': {P.progress},'steps': {P.steps},'speed': {P.speed},'steering_angle': {P.steering_angle},'track_width':  {P.track_width},'waypoints': {P.waypoints},'closest_waypoints':{P.closest_waypoints},'is_offtrack': {P.is_offtrack}}}")
+    print(f"{{'all_wheels_on_track': {P.all_wheels_on_track},'x':{P.x},'y':{P.y},'distance_from_center': {P.distance_from_center},'is_left_of_center': {P.is_left_of_center},'heading': {P.heading},'progress': {P.progress},'steps': {P.steps},'speed': {P.speed},'steering_angle': {P.steering_angle},'track_width':  {P.track_width},'waypoints':self.waypoints ,'closest_waypoints':{P.closest_waypoints},'is_offtrack': {P.is_offtrack}}}")
      
 def dist_to_racing_line(closest_coords, second_closest_coords, car_coords):    
         # Calculate the distances between 2 closest racing points
