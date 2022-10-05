e8bc63c2e02554c4cbe4b52412ed25383b933dd1
Neo Alienson <neo@01man.com>
2022-10-05 10:30:00 +0800

    update

---
+++ download_logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm ../data/logs/training-simtrace/*.csv
+rm ../data/logs/training-simtrace/*.dsv
+
+pushd ../data/logs/training-simtrace
+aws $DR_LOCAL_PROFILE_ENDPOINT_URL s3 sync s3://bucket/rl-deepracer-sagemaker/training-simtrace/  .
+find . -name "*.csv" -exec sh -c 'f="{}"; mv -- "$f" "${f%.csv}.dsv"' \;
+aws $DR_LOCAL_PROFILE_ENDPOINT_URL s3 sync s3://bucket/rl-deepracer-1/training-simtrace/  .
+find . -name "*.csv" -exec sh -c 'f="{}"; mv -- "$f" "${f%.csv}.esv"' \;
+aws $DR_LOCAL_PROFILE_ENDPOINT_URL s3 sync s3://bucket/rl-deepracer-2/training-simtrace/  .
+find . -name "*.csv" -exec sh -c 'f="{}"; mv -- "$f" "${f%.csv}.fsv"' \;
+
+ls -vrt | cat -n | while read n f; do mv -n "$f" "$n-iteration.csv"; done 
+
+ls -rlt
+popd
