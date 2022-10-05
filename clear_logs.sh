0dd5c7d51c4afd1b6ac526624095975212f109d3
Neo Alienson <neo@01man.com>
2022-10-06 06:58:02 +0800

    update

--- clear_logs.sh
+++ clear_logs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
+aws $DR_LOCAL_PROFILE_ENDPOINT_URL s3 rm --recursive s3://bucket/rl-deepracer-1
 rm ../data/logs/training-simtrace/*.csv
-aws $DR_LOCAL_PROFILE_ENDPOINT_URL s3 rm --recursive s3://bucket/rl-deepracer-*
