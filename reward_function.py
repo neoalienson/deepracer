9d60f487ec113bce2d466de6d2c622dac41fe2b8
Neo Alienson <neo@01man.com>
2022-10-09 04:50:22 +0800

    update

--- reward_function.py
+++ reward_function.py
@@ -303,7 +303,7 @@ def get_immediate_reward():
     # elif SETTINGS.STAGE == 2:
     #     lc = (REWARDS.speed + REWARDS.distance) ** 2 + ( REWARDS.speed * REWARDS.distance)
     # else:
-    lc = REWARDS.distance * 2 + REWARDS.speed + 0.5
+    lc = REWARDS.distance
 #    lc = (REWARDS.speed + REWARDS.distance + REWARDS.heading) ** 2 + ( REWARDS.speed * REWARDS.distance * REWARDS.heading)
 
     ## Stage 1 Checks
